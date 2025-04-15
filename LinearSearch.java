5c022e19305197dde3e982c069e64e8bb0020ec1
muhammednihal1 <muhammednihall@proton.me>
2025-04-15 11:51:00 +0530

    completed

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,31 +1,37 @@
 import java.util.Scanner;
-public class LinearSearch {
-public static int linearSearch(int[] arr, int target) {
-for (int i = 0; i < arr.length; i++) {
-if (arr[i] == target) {
-return i;
-}
-}
-return -1;
-}
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-System.out.print("Enter the limit (size) of the array: ");
-int limit = scanner.nextInt();
-int[] arr = new int[limit];
+
+class Linear {
+public static void main(String args[]) {
+int a, i, e;
+boolean found = false;
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the number of elements in array:");
+a = sc.nextInt();
+
+int[] ar = new int[a];
+
 System.out.println("Enter the elements of the array:");
-for (int i = 0; i < limit; i++) {
-arr[i] = scanner.nextInt();
+for (i = 0; i < a; i++) {
+ar[i] = sc.nextInt();
 }
-System.out.print("Enter the element to search: ");
-int target = scanner.nextInt();
-int result = linearSearch(arr, target);
-if (result != -1) {
-System.out.println("Element " + target + " found at index " + result);
-} else {
-System.out.println("Element " + target + " not found in the array");
-}
-scanner.close();
+
+System.out.println("Enter the element to be searched:");
+e = sc.nextInt();
+
+for (i = 0; i < a; i++) {
+if (ar[i] == e) {
+found = true;
+break;
 }
 }
 
+if (found) {
+System.out.println("Element is found");
+} else {
+System.out.println("Element is not found");
+}
+
+sc.close();
+}
+}
