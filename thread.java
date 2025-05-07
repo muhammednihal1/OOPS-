4eee89285d6079ec6260beb570e8d3fb1f2a76da
MUHAMMED NIHAL <muhammednihall@proton.me>
2025-05-07 15:41:39 +0530

    Add files via upload

---
+++ thread.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class MultiplicationTable extends Thread {
+public void run() {
+System.out.println("Multiplication Table of 5:");
+for (int i = 1; i <= 10; i++) {
+System.out.println("5 x " + i + " = " + (5 * i));
+}
+}
+}
+class PrimeNumbers extends Thread {
+public void run() {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter the value of N: ");
+int n = scanner.nextInt();
+System.out.println("First " + n + " prime numbers:");
+int count = 0;
+int num = 2;
+while (count < n) {
+boolean isPrime = true;
+for (int i = 2; i <= Math.sqrt(num); i++) {
+if (num % i == 0) {
+isPrime = false;
+break;
+}
+}
+if (isPrime) {
+System.out.print(num + " ");
+count++;
+}
+num++;
+}
+System.out.println(); 
+scanner.close();
+}
+}
+public class MainThread {
+public static void main(String[] args) {
+MultiplicationTable tableThread = new MultiplicationTable();
+PrimeNumbers primeThread = new PrimeNumbers();
+tableThread.start();
+primeThread.start();
+}
+}
+
